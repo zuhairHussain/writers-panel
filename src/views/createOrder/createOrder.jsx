f765a013f42ccb2dd8f0d5a57c110a720ad06ecb
zuhair.hussain <zuhair.hussain@gaditek.com>
2020-06-26 21:13:38 +0500

    Change UI of order page

--- src/views/createOrder/createOrder.jsx
+++ src/views/createOrder/createOrder.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import Container from '../../includes/container';
 import './createOrder.scss';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Card = (props) => {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            padding: '25px',
+            textAlign: 'left',
+            color: '#000',
+        },
+    }));
+    const classes = useStyles();
+    return (
+        <Paper className={classes.paper}>{props.children}</Paper>
+    )
+};
 
 export default class createOrder extends Component {
     constructor(props) {
@@ -11,14 +30,21 @@ export default class createOrder extends Component {
         }
     }
     render() {
-        const { } = this.state;
+
         return (
-            <Container className="acc-billing-wrapper">
+            <Container className="order-wrapper">
                 <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <Grid item xs={8}>
+                        <Card>
+                            <h4 class="mb-1 hed">PLACE AN ORDER</h4>
+                            <p class="mb-5 sub-hed">It's fast, secure and confidential</p>
+
+                            <p class="mb-3 sec-label">Paper Details</p>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        </Card>
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item xs={4}>
+                        <Card>test</Card>
                     </Grid>
                 </Grid>
             </Container>
