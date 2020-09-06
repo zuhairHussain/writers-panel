c23c82df236ea43688179d9011d30f5d10898b24
zuhair.hussain <zuhair.hussain@gaditek.com>
2020-09-06 18:35:43 +0500

    Adde build folder

---
+++ build/precache-manifest.6c87ff953033fd80ef3d1e4f9855e25c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f04381f00954c09671d48f99bb7afeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3e51fd06f91711a69b",
+    "url": "/static/css/main.1fc2bb28.chunk.css"
+  },
+  {
+    "revision": "2499f7a1021c4158a82f",
+    "url": "/static/js/2.f6d4bd86.chunk.js"
+  },
+  {
+    "revision": "1b3e51fd06f91711a69b",
+    "url": "/static/js/main.d92c7a8d.chunk.js"
+  },
+  {
+    "revision": "d82fe5237ceff9b0cedd",
+    "url": "/static/js/runtime-main.6bc1180a.js"
+  },
+  {
+    "revision": "ebea168310becc3d4aeaea87c0cb9e25",
+    "url": "/static/media/empty-state.ebea1683.png"
+  },
+  {
+    "revision": "77b00da1122ce0acf2abdfec73cc8eba",
+    "url": "/static/media/order-create.77b00da1.svg"
+  },
+  {
+    "revision": "645b3e01287b85405ead1d58f259f8ce",
+    "url": "/static/media/order-listing.645b3e01.svg"
+  }
+]);
